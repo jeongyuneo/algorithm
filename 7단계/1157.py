eca641d6b25728cf2697b4d13894c668bb1c5ec7
jeongyuneo <piq2255@gmail.com>
2021-01-25 09:47:09 +0900

    step 7 1152

---
+++ 7단계/1157.py
@@ -0,0 +1,13 @@
+a = input().upper()
+b = list(0 for i in range(27))
+for i in a:
+    b[ord(i) - 65] += 1
+tmp_list = []
+maxB = max(b)
+for i in range(len(b)):
+    if maxB == b[i]:
+        tmp_list.append(i)
+if len(tmp_list) == 1:
+    print(chr(tmp_list[0] + 65))
+else:
+    print('?')
