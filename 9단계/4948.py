f8be6df345e12fcec860f223efd2d0d856df1b5b
jeongyuneo <piq2255@gmail.com>
2021-08-31 06:57:02 +0900

    step9

---
+++ 9단계/4948.py
@@ -0,0 +1,17 @@
+def isPrime(n):
+    c = [1]*(n+1)
+    c[0], c[1] = 0, 0
+    for i in range(2, int(math.sqrt(n)) + 1):
+        j = 2
+        if c[i]:
+            while i*j <= n:
+                c[i*j] = 0
+                j += 1
+    return c
+
+import math
+n = int(input())
+while n != 0:
+    res = isPrime(n*2)
+    print(sum(res[n+1:n*2+1]))
+    n = int(input())
