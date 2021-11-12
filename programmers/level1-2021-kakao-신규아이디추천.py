ddc68e61b7de692cb40d0c5541f82d541d6c9760
jeongyuneo <piq2255@gmail.com>
2021-11-12 23:14:05 +0900

    신규 아이디 추천 개선

--- programmers/level1-2021-kakao-신규아이디추천.py
+++ programmers/level1-2021-kakao-신규아이디추천.py
@@ -1,33 +1,20 @@
 import re
 
-allowed_strings = re.compile('[a-z|0-9|\-|_|.]')
-repeated_dot = re.compile('[.]+')
-beginnings = re.compile('^[.].*')
-endings = re.compile('.*[.]$')
+allowed_strings = re.compile('[a-z0-9\-_.]')
+repeated_dot = re.compile('\.+')
+beginnings_and_endings = re.compile('^[.]|[.]$')
 
 def solution(new_id):
     new_id_round_1 = new_id.lower()
-    new_id_round_2 = ''
-    for idx in allowed_strings.findall(new_id_round_1):
-        new_id_round_2 += idx
+    new_id_round_2 = ''.join(idx for idx in allowed_strings.findall(new_id_round_1))
     new_id_round_3 = repeated_dot.sub('.', new_id_round_2)
-    new_id_round_4 = new_id_round_3
-    if beginnings.match(new_id_round_3):
-        new_id_round_4 = new_id_round_4[1:]
-    if endings.match(new_id_round_3):
-        new_id_round_4 = new_id_round_4[:-1]
-    new_id_round_5 = new_id_round_4
-    if new_id_round_5 == '':
-        new_id_round_5 = 'a'
-    new_id_round_6 = new_id_round_5
-    if len(new_id_round_6) >= 16:
-        new_id_round_6 = new_id_round_6[:15]
-        if endings.match(new_id_round_6):
-            new_id_round_6 = new_id_round_6[:-1]
+    new_id_round_4 = beginnings_and_endings.sub('', new_id_round_3)
+    new_id_round_5 = new_id_round_4 if new_id_round_4 != '' else 'a'
+    new_id_round_6 = beginnings_and_endings.sub('', new_id_round_5[:15])
     new_id_round_7 = new_id_round_6
     if len(new_id_round_7) <= 2:
         while len(new_id_round_7) != 3:
-            new_id_round_7 += new_id_round_7[len(new_id_round_7)-1]
+            new_id_round_7 += new_id_round_7[-1]
     answer = new_id_round_7
     return answer
 
@@ -38,5 +25,6 @@ a = [
     "123_.def",
     "abcdefghijklmn.p"
 ]
+
 for x in a:
     print(solution(x))
