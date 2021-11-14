52f394c8222ec9a98c60d749bf4a165c2b3c29d3
jeongyuneo <piq2255@gmail.com>
2021-11-14 23:42:51 +0900

    로또의 최고 순위와 최저 순위 개선

--- programmers/level1-2021-devmatching-로또의최고순위와최저순위.py
+++ programmers/level1-2021-devmatching-로또의최고순위와최저순위.py
@@ -1,28 +1,13 @@
 def solution(lottos, win_nums):
     answer = []
     cnt = 0
+    rank = [6, 6, 5, 4, 3, 2, 1]
     for idx in lottos:
         if idx in win_nums:
             cnt += 1
-    best_cnt, worst_cnt = cnt+lottos.count(0), cnt
-    best_ranking, worst_ranking = get_ranking(best_cnt), get_ranking(worst_cnt)
-    answer.append(best_ranking)
-    answer.append(worst_ranking)
+    best_ranking, worst_ranking = rank[cnt+lottos.count(0)], rank[cnt]
+    answer.extend([best_ranking, worst_ranking])
     return answer
-
-def get_ranking(cnt):
-    if cnt == 6:
-        return 1
-    elif cnt == 5:
-        return 2
-    elif cnt == 4:
-        return 3
-    elif cnt == 3:
-        return 4
-    elif cnt == 2:
-        return 5
-    else:
-        return 6
 
 a = [
     [[44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]],
