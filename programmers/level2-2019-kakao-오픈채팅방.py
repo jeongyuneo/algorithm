e2be9ecaeebcc338d90ee760c370a5aca29e14c2
jeongyuneo <piq2255@gmail.com>
2021-11-15 20:05:01 +0900

    오픈채팅방 개선

--- programmers/level2-2019-kakao-오픈채팅방.py
+++ programmers/level2-2019-kakao-오픈채팅방.py
@@ -3,21 +3,15 @@ from collections import defaultdict
 def solution(record):
     answer = []
     user_dict = defaultdict(str)
-    split_record = []
-    for i in record:
-        if 'Change' in i or 'Enter' in i:
-            act, user_id, user_nickname = i.split(" ")
-            split_record.append([act, user_id, user_nickname])
-        else:
-            act, user_id = i.split(" ")
-            split_record.append([act, user_id])
-            continue
+    for idx in record:
+        if idx.startswith('Enter') or idx.startswith('Change'):
+            act, user_id, user_nickname = idx.split(" ")
         user_dict[user_id] = user_nickname
-    for i in split_record:
-        if 'Enter' in i:
-            answer.append("%s님이 들어왔습니다." % user_dict.get(i[1]))
-        elif 'Leave' in i:
-            answer.append("%s님이 나갔습니다." % user_dict.get(i[1]))
+    for idx in record:
+        if idx.startswith('Enter'):
+            answer.append("%s님이 들어왔습니다." % user_dict.get(idx.split()[1]))
+        elif idx.startswith('Leave'):
+            answer.append("%s님이 나갔습니다." % user_dict.get(idx.split()[1]))
     return answer
 
 a = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
