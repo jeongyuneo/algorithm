21e27c3c25d4f4beaa7904dac5326a4e96ddd666
jeongyuneo <piq2255@gmail.com>
2021-12-12 01:36:30 +0900

    올림픽

--- boj/알고리즘별/정렬_Sorting/8979.py
+++ boj/알고리즘별/정렬_Sorting/8979.py
@@ -1,7 +1,7 @@
 def count_value(li, key):
     count = 0
     for value in li:
-        if value[1] == key:
+        if value[1][:3] == key:
             count += 1
     return count
 
@@ -13,11 +13,15 @@ for i in range(n):
 res = sorted(nations.items(), key=lambda x: (x[1][0], x[1][1], x[1][2]), reverse=True)
 i, ranking = 0, 1
 while i < n:
-    if count_value(res, res[i][1]) == 1:
+    count = count_value(res, res[i][1])
+    if count == 1:
         res[i][1].append(ranking)
         ranking += 1
     else:
-        res[i][1].append(ranking)
+        for j in range(count):
+            res[i+j][1].append(ranking)
+        i += count-1
+        ranking += count
     i += 1
 res_dict = {}
 for i in range(n):
